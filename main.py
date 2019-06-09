be0bfd4fd659c45c5b70913a71b7cabd9d4cc688
Jonny1117 <51047241+Jonny1117@users.noreply.github.com>
2019-06-09 16:19:50 +0300

    Add files via upload
    я прописал все состояния, проверьте, все ли я указал?

--- main.py
+++ main.py
@@ -48,3 +48,59 @@ def main_handler(msg):
             )
         elif text == '/help':
             bot.send_message(chat_id, Const.msg_help_text, parse_mode = 'HTML')
+        else:
+            #Все состояния:
+            #primary_state
+            #first_choice
+            #create_playlist
+            #fill_playlis
+            #choice_playlist
+            #playlist_menu
+            #rename_playlist
+            #extend_playlist
+            #approve_del_playlist
+            #choice_track
+            #track_menu
+            #approve_del_track
+
+        state = utils.return_state(chat_id)
+
+        if state == 'primary_state':
+            primary_state(msg, text, chat_id)
+
+        elif state == 'first_choice':
+            first_choice(msg, text, chat_id)
+
+        elif state == 'create_playlist':
+            create_playlist(msg, text, chat_id)
+
+        elif state == 'fill_playlis':
+            fill_playlis(msg, text, chat_id)
+
+        elif state == 'choice_playlist':
+            choice_playlist(msg, text, chat_id)
+
+        elif state == 'playlist_menu':
+            playlist_menu(msg, text, chat_id)
+
+        elif state == 'rename_playlist':
+            rename_playlist(msg, text, chat_id)
+
+        elif state == 'extend_playlist':
+            first_choice(msg, text, chat_id)
+
+        elif state == 'approve_del_playlist':
+            approve_del_playlist(msg, text, chat_id)
+
+        elif state == 'choice_track':
+            first_choice(msg, text, chat_id)
+
+        elif state == 'track_menu':
+            track_menu(msg, text, chat_id)
+
+        elif state == 'first_choice':
+            approve_del_track(msg, text, chat_id)
+
+    except Exception as e:
+        raise e
+        print(traceback.format_exc())
